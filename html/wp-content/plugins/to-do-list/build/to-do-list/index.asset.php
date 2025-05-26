fc829fc6ee8deb65b63d1fcf03f6149113053ae6
Jeswin <jlibay1053538@gmail.com>
2025-05-27 02:26:07 +0800

    changed structure

---
+++ html/wp-content/plugins/to-do-list/build/to-do-list/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '832f6a86db85a0f09d9f');
---
+++ html/wp-content/plugins/to-do-list/build/to-do-list/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '7ab60c41c101c354a250');
