6ad3a7cc6afef3fdfd95cdede614ade4f12f1492
Jeswin <jlibay1053538@gmail.com>
2025-06-08 23:34:49 +0800

    responsive hero

--- plugins/block-playstore/src/block-games-line/view.js
+++ plugins/block-playstore/src/block-games-line/view.js
@@ -14,6 +14,5 @@ document.addEventListener("DOMContentLoaded", () => {
 		grabCursor: true,
 		mousewheelControl: true,
 		keyboardControl: true,
-		spaceBetween: 8,
 	});
 });
--- plugins/block-playstore/src/block-hero/view.js
+++ plugins/block-playstore/src/block-hero/view.js
@@ -1,17 +1,16 @@
-import Swiper from "swiper";
-import "swiper/css";
-import "swiper/css/navigation";
+import Swiper from "swiper/bundle";
 
 // DOM ready (in case your block appears multiple times)
 document.addEventListener("DOMContentLoaded", () => {
 	var swiperHero = new Swiper(".partner-swiper", {
 		loop: true,
 		autoplay: {
-			delay: 2000,
+			delay: 0,
 			disableOnInteraction: false,
 		},
+		freeMode: true,
 		slidesPerView: "auto",
-		speed: 1500,
+		speed: 3500,
 		grabCursor: true,
 		mousewheelControl: true,
 		keyboardControl: true,
